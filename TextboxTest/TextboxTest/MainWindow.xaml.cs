f20825a0c171a269592ca194f23577357bd52be4
Auke Swen <aukeswen2001@yahoo.com>
2018-03-09 14:59:46 +0100

    Idea for numberbox

---
+++ TextboxTest/TextboxTest/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextboxTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool komma = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private bool IsTextAllowed(string text)
+        {
+            
+            if (komma)
+            {
+                Regex regex = new Regex("([0-9])");
+                return regex.IsMatch(text);
+            }
+            else
+            {
+                komma = new Regex(",").IsMatch(text);
+                Regex regex = new Regex("([0-9,])");
+                return regex.IsMatch(text);
+            }
+        }
+    }
+}
